9df282bde40ea247535cd5c506ae5ba7eae4cc96
xdlomad <cheanmol@gmail.com>
2023-04-10 15:32:47 +0800

    login and register api with token

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const jwt = require('jsonwebtoken');
+
 const app = express()
 const port = 3000
 //Database of users
@@ -25,15 +27,23 @@ let dbUsers = [
 
 app.use(express.json());
 
+
 app.get('/hello', (req, res) => {
-    res.send('Hello World!')
+  res.send("hello world")
+})
+
+app.get('/verify', verifyToken, (req, res) => {
+    console.log(req.user)
+
   })
 
 app.post('/login', (req, res) => {
     let data = req.body
     // res.send(' Post request '+ JSON.stringify(data));
     //res.send(' Post request '+ data.name +data.password)
-    res.send(login(data.username,data.password));
+    //res.send(login(data.username,data.password));
+    const loginuser = login(data.username, data.password);
+    res.send(generateToken(loginuser))
   });
 
 app.post('/register',(req, res)=>{
@@ -78,9 +88,26 @@ function register(newusername, newpassword, newname, newemail) {
       }  
   }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  function generateToken(loginProfile){
+    return jwt.sign(loginProfile, 'very_long_long_long_long_numbers_alphabets_lmao_stillgoing_password', { expiresIn: '1h' });
+  }
 
+  function verifyToken(req, res, next){
+    //let token = req.headers['authorization'].split(' ')[1];
+    let header = req.headers.authorization
+    let token = header.split(' ')[1]
+    jwt.verify(token,'very_long_long_long_long_numbers_alphabets_lmao_stillgoing_password',function(err,decoded){
+      if(err) {
+        res.send("invalid token")
+      }
+      req.user = decoded // bar
+
+      next()
+    });
+  }
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
 
